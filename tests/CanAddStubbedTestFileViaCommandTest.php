ed4727eb49763e1374227718b62e64a78b254266
Craig <craig.paul@coconutsoftware.com>
2023-11-25 22:29:12 -0600

    Skips test for specific Laravel versions due to functionality not being available

--- tests/CanAddStubbedTestFileViaCommandTest.php
+++ tests/CanAddStubbedTestFileViaCommandTest.php
@@ -13,6 +13,10 @@ class CanAddStubbedTestFileViaCommandTest extends TestCase
 
     public function testCanAddStubbedTestFileWhenAskedForTestNameSuccessfully()
     {
+        if (version_compare($this->app->version(), '9.49', '<')) {
+            return $this->markTestSkipped('Prompting for missing input was not available until Laravel v9.49.0');
+        }
+
         $this->artisan('blitz:make')->expectsQuestion('What should the test be named?', 'AnotherExampleLoadTest')->assertOk();
 
         $this->assertFileExists($this->app->basePath('tests/Blitz/AnotherExampleLoadTest.php'));
