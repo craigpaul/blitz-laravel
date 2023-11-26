3a607326205915f8cee669fd9eff3a23ee882d46
Craig <craig.paul@coconutsoftware.com>
2023-11-25 21:48:24 -0600

    Enables the command to run when running in console

--- src/BlitzServiceProvider.php
+++ src/BlitzServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace CraigPaul\Blitz;
 
+use CraigPaul\Blitz\Console\Commands\TestMakeCommand;
 use Illuminate\Support\ServiceProvider;
 
 class BlitzServiceProvider extends ServiceProvider
@@ -14,5 +15,11 @@ class BlitzServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                TestMakeCommand::class,
+            ]);
+        }
     }
 }
