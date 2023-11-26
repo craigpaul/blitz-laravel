08feb19b4c4617a52d09eb500de466966abef1a3
Craig <craig.paul@coconutsoftware.com>
2023-11-25 21:24:53 -0600

    Adds controller logic and route defintion for generating targets based on an existing workflown namespace

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use CraigPaul\Blitz\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
 Route::get('_blitz/workflows')->name('blitz.workflows')->uses([Controllers\WorkflowController::class, 'index']);
+Route::post('_blitz/targets')->name('blitz.targets')->uses([Controllers\TargetController::class, 'store']);
---
+++ src/Http/Controllers/TargetController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace CraigPaul\Blitz\Http\Controllers;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class TargetController
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @param \Illuminate\Contracts\Foundation\Application $app
+     * @param \Illuminate\Contracts\Routing\ResponseFactory $response
+     *
+     * @return void
+     */
+    public function __construct(
+        protected Application $app,
+        protected ResponseFactory $response,
+    ) {
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $instance = $this->app->make($request->post('namespace'));
+
+        $instance->setUp();
+
+        return $this->response->json($instance->getTargets());
+    }
+}
