d8185ee8c8b193d4dec772658c75535e609f3fbb
Craig <craig.paul@coconutsoftware.com>
2023-11-26 20:57:14 -0600

    Adds docblock describing new configuration option

--- config/blitz.php
+++ config/blitz.php
@@ -2,6 +2,18 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Blitz Master Switch
+    |--------------------------------------------------------------------------
+    |
+    | This option may be used to disable all Blitz endpoints, which simply
+    | provides a single and convenient way to enable or disable access
+    | to your load testing workflows and the ability to generate
+    | the targets necessary to run those workflows.
+    |
+    */
+
     'enabled' => env('BLITZ_ENABLED', false),
 
 ];
