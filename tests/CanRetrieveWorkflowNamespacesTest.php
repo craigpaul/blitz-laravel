c0bb3c82e61c0621fca799cb04e75f89aededfae
Craig Paul <craig.paul91@gmail.com>
2024-03-28 21:50:12 -0600

    Adds test case for retrieving existing workflow namespaces

---
+++ tests/CanRetrieveWorkflowNamespacesTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CraigPaul\Blitz\Tests;
+
+use Illuminate\Testing\Fluent\AssertableJson;
+
+class CanRetrieveWorkflowNamespacesTest extends TestCase
+{
+    public function testCanRetrieveExistingWorkflowNamespaces()
+    {
+        $this->withoutExceptionHandling()
+            ->get(route('blitz.workflows'))
+            ->assertOk()
+            ->assertJsonCount(1)
+            ->assertJson(fn (AssertableJson $json) => $json->where('0', 'Tests\\Blitz\\ExampleTest'));
+    }
+}
