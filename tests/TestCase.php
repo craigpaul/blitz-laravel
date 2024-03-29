5d9a6bd1cecac93f865141983bfc17fcdcec6a1c
Craig Paul <craig.paul91@gmail.com>
2024-03-28 21:50:12 -0600

    Adds environment setup for test cases

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,45 @@
 
 namespace CraigPaul\Blitz\Tests;
 
+use CraigPaul\Blitz\BlitzServiceProvider;
+use Illuminate\Support\Facades\Storage;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    //
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Storage::build($this->app->basePath('tests/Blitz'))->put('ExampleTest.php', file_get_contents(__DIR__ . '/stubs/ExampleTest.php.stub'));
+    }
+
+    /**
+     * Clean up the testing environment before the next test.
+     *
+     * @return void
+     */
+    protected function tearDown(): void
+    {
+        Storage::build($this->app->basePath('tests'))->deleteDirectory('Blitz');
+
+        parent::tearDown();
+    }
+
+    /**
+     * Get package providers.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [BlitzServiceProvider::class];
+    }
 }
